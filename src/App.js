5634c6e6868638269a490e635cf853110217f4f8
Claire Bian <clairekb248@gmail.com>
2025-06-16 09:57:58 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', paddingTop: '100px' }}>
+      <h1>Hello, I'm Claire</h1>
+      <p>Welcome to my personal website. Coming soon!</p>
+    </div>
+  );
+}
+
+export default App;
